444701106cecc5581183f5c55721189eaa201b2a
Abel Mekonnen <abelmekonen247@gmail.com>
2026-05-10 00:16:18 +0300

    feat: implement JavaFX login UI with controller and base styling

--- src/main/java/com/auction/client/controllers/LoginController.java
+++ src/main/java/com/auction/client/controllers/LoginController.java
@@ -1,11 +1,24 @@
 package com.auction.client.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class LoginController {
 
+    @FXML private TextField usernameField;
+    @FXML private PasswordField passwordField;
+
     @FXML
     public void initialize() {
-        // TODO: bind login UI events, RMI login call, view transition based on role
+        // TODO: setup RMI connection check
+    }
+
+    @FXML
+    private void handleLogin() {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+        System.out.println("Login attempted for: " + username);
+        // TODO: call RMI login and transition view
     }
 }
